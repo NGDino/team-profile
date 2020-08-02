d22c7eed0266b95511d098c7499554c721616aa7
D-Know <n.dino90@gmail.com>
2020-08-02 08:28:38 -0700

    questions specific to roles working.  Adds members to team array

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const {
     inflateRawSync
 } = require('zlib');
 const Engineer = require('./lib/Engineer');
+const Employee = require('./lib/Employee');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager')
 
 
 const team = []
@@ -33,36 +36,70 @@ function getInfo() {
             }
         ])
         .then(answers => {
-            console.log(answers);
+            
 
             
             if (answers.role === 'Engineer') {
-                const someEngineer = new Engineer(answers.name, answers.email, answers.id, answers.role)
-                console.log(someEngineer.getName())
-                 console.log(someEngineer.getEmail())
-                team.push(someEngineer);
-            }else{console.log('Picked a different dude!')}
+                inquirer.prompt([{
+                    type: 'input',
+                    name: 'gitHub',
+                    message: "Enter Engineer's GitHub"
+                }])
+                .then(ans => {
+                    console.log(ans.gitHub)
                 
-            // const someEngineer = new Engineer(answers.name, answers.email, answers.id, answers.role)
-            // console.log(someEngineer.getName())
-            // console.log(someEngineer.getType())
-            // team.push(someEngineer);
-            // inquirer.prompt([{
-            //         type: 'list',
-            //         name: 'choice',
-            //         message: "Want to add more?",
-            //         choices: ["Manager", "Engineer", "Intern", "Done"]
+                    const someEngineer = new Engineer(answers.name, answers.email, answers.id, answers.role, ans.gitHub )
+                    team.push(someEngineer);
+                    addMore()
+                })
+            }else if(answers.role === 'Manager') {
+                    inquirer.prompt([{
+                        type: 'input',
+                        name: 'office',
+                        message: "Enter Managers office number"
+                    }])
+                    .then(ans => {
+                        console.log(ans.office)
+                    
+                        const someManager = new Manager(answers.name, answers.email, answers.id, answers.role, ans.office )
+                        team.push(someManager);
+                        addMore()
+                    })
+                }else if(answers.role === 'Intern') {
+                        inquirer.prompt([{
+                            type: 'input',
+                            name: 'school',
+                            message: "Enter Interns school"
+                        }])
+                        .then(ans => {
+                            console.log(ans.school)
+                        
+                            const someIntern = new Intern(answers.name, answers.email, answers.id, answers.role, ans.school )
+                            team.push(someIntern);
+                            addMore()
+                        })
+            }else{
+                const someEmployee = new Employee(answers.name, answers.email, answers.id, answers.role)
+                team.push(someEmployee)
+                addMore()
+            }
 
-            //     }])
-                // .then(choice => {
-                //     if (choice.choice === 'Engineer') {
-                //         getInfo();
-                //     } else if (choice.choice === 'Intern') {
-                //         getInfo();
-                //     } else {
-                //         console.log(team);
-                //     }
-                // })
+            function addMore() {
+                inquirer.prompt([{
+                    type: 'confirm',
+                    name: 'addNew',
+                    message: 'Would you Like to add another team member?'
+                }])
+                .then(res =>{
+                    if (res.addNew === true){
+                        getInfo(team)
+                    }else{
+                        console.log(team)
+                    }
+                })
+                
+            }
+                
         })
 }
 
